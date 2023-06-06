c191f0306eb3f1c7ad1017dd2f6e4432ebb509a5
Doug McQuinn <wdmcquinn@gmail.com>
2023-06-06 16:12:22 -0400

    Update for Ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 sudo apt update -qq
 
 ## make sure ansible is installed
-sudo apt install ansible git -y
+sudo apt install ansible git flatpak -y
+
+
+## Install ansible roles from galaxy
+ansible-galaxy install artis3n.tailscale
+
 
 
