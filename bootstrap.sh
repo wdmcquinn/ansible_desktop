914975f9070878623b673f438db931f9b9f5829f
Doug McQuinn <wdmcquinn@gmail.com>
2024-11-06 16:37:08 -0500

    Update bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@
 sudo apt update -qq
 
 ## make sure ansible is installed
-sudo apt install ansible git flatpak -y
+sudo apt install ansible git
 
+## Do initial Pull
+ansible-pull -o -U https://github.com/wdmcquinn/ansible_desktop.git
 
 
 
